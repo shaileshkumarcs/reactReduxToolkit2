a2a52500359da926c37fc74739971983fbd16e1e
Shailesh Dangi <shailesh@bni.com>
2023-11-08 15:50:24 +0530

    HTTP call with redux

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { cartActions } from '../../store/cart-slice';
 
@@ -6,7 +6,6 @@ import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  // const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
